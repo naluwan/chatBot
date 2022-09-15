d8691e744d4c5e614c78f25bd4964a6088c70541
naluwan <street.baller06@gmail.com>
2022-09-15 15:41:24 +0800

    feat: update form action

--- routes/pages/modules/admin.js
+++ routes/pages/modules/admin.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const adminController = require('../../../controllers/pages/admin-controller')
 
-router.put('/stories/:action', adminController.putResponse)
+router.put('/stories/response/:action', adminController.putResponse)
 router.get('/stories', adminController.getStories)
 router.patch('/users/:id', adminController.patchUser)
 router.get('/users', adminController.getUsers)
