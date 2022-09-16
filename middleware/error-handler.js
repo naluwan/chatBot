735aca8eb96c79ad2a3d2b87e72ff74136e2ea5a
naluwan <street.baller06@gmail.com>
2022-09-16 16:13:22 +0800

    feat: add api error handler middleware

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -7,5 +7,20 @@ module.exports = {
     }
     res.redirect('back')
     next(err)
+  },
+  apiErrorHandler(err, req, res, next) {
+    console.log(typeof err)
+    if (err instanceof Error) {
+      res.status(err.status || 500).json({
+        status: 'error',
+        message: `${err.name}: ${err.message}`
+      })
+    } else {
+      res.status(500).json({
+        status: 'error',
+        message: `${err}`
+      })
+    }
+    next(err)
   }
 }
