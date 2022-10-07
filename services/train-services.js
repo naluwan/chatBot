9cf28591d0f806fb8b32b78780d851db9f3f3683
naluwan <street.baller06@gmail.com>
2022-10-07 11:15:44 +0800

    feat: modify getAllTrainData

--- services/train-services.js
+++ services/train-services.js
@@ -40,28 +40,6 @@ const trainServices = {
         ).stories
         const domain = JSON.parse(data.filter(item => item.name === 'domain')[0].content)
         const nlu = JSON.parse(data.filter(item => item.name === 'nlu-json')[0].content)
-        const responses = domain.responses
-
-        stories.map(story => {
-          return story.steps.map(step => {
-            // 抓取機器人回覆
-            if (step.action) {
-              step.response = JSON.parse(
-                JSON.stringify(responses[step.action][0].text).replace(/ \\n/g, '\\r')
-              )
-            }
-            // 抓取使用者例句
-            if (step.intent) {
-              const examples = nlu.rasa_nlu_data.common_examples.filter(
-                nluItem => nluItem.intent === step.intent && nluItem.text !== step.intent
-              )
-
-              const currentExample = examples.map(example => example.text)
-              step.examples = currentExample
-            }
-            return step
-          })
-        })
         cb(null, { stories, domain, nlu })
       })
       .catch(err => cb(err))
