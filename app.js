130485615fc3846468b6820258f37bcfe3fd50ad
naluwan <street.baller06@gmail.com>
2023-03-07 16:00:01 +0800

    feat: add json and urlencoded limit

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ app.engine('hbs', hbs({ extname: '.hbs', helpers: handlebarsHelpers }))
 app.set('view engine', 'hbs')
 app.use(express.static('public'))
 app.use('/upload', express.static(path.join(__dirname, 'upload')))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.use(express.json({ limit: '50mb' }))
+app.use(express.urlencoded({ extended: true, limit: '50mb' }))
 app.use(
   cors({
     credentials: true,
